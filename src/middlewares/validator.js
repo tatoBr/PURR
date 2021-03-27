834ec5fa13174f32d16508841e405c2dd3e8a0f2
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-03-27 12:32:33 -0300

    add subscribe validation schema

---
+++ src/middlewares/validator.js
@@ -0,0 +1,38 @@
+const Joi = require('joi');
+const { httpStatusCode:{ BAD_REQUEST }} = require( '../utils/constants' );
+const ServerResponse = require('../utils/serverResponse');
+
+module.exports = {
+    validateBody: schema => {
+        return ( req, res, next )=>{
+            const result = schema.validate( req.body );
+            if( result.error ) 
+                return res.status( BAD_REQUEST ).json( new ServerResponse( ServerResponse.INVALID_REQUEST_PROPERTY_MSG, result.error ))
+            
+            if( !req.value ) req.value = {};
+            req.value.body = result.value;
+            
+            next()
+        }
+    },
+    schemas:{
+        subscribe: Joi.object().keys({
+            username: Joi.string()
+                .required()
+                .trim()
+                .min(3)
+                .max(24)
+                .regex(/^\w+(?:[^\w,]+\w+)*[^,\w]*$/),
+            email: Joi.string()
+                .required()
+                .trim()
+                .lowercase({ force: true })
+                .email(),
+            password: Joi.string()
+                .required()
+                .trim()
+                .min(8)
+                .max(24)                
+        }),
+    }
+}
