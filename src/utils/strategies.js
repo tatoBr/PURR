8207917de7bfc058a2100335b2adc46734ecf0bf
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-03-29 08:37:39 -0300

    add jwt and local passport strategy

--- src/utils/strategies.js
+++ src/utils/strategies.js
@@ -1,27 +1,56 @@
 const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt');
+const { Strategy: LocalStrategy } = require( 'passport-local')
+
 const { ACCESS_TOKEN_SECRET } = require('../config/index');
 const UserModel = require('../models/user');
 
 //JSON WEB TOKENS STRATEGY
-let options = {
+let jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: ACCESS_TOKEN_SECRET       
+    secretOrKey: ACCESS_TOKEN_SECRET,
+    passReqToCallback: true       
 };
 
-let verifyCallback = async( payload, done )=>{
+/**
+ * 
+ * @param {*} payload 
+ * @param { Function } done 
+ */
+let jwtVerifyCallback = async( req, payload, done )=>{
     try {
         const user = await UserModel.findById( payload.sub );
         if(!user) return done( null, false );
-        //req.user = user;
-        done( null, user );
+        req.user = user;
+        return done( null, user );
     } catch (error) {
         done( error, false );
     }
 };
 
-let jwtStrategy = new JwtStrategy( options, verifyCallback );
+let jwtStrategy = new JwtStrategy( jwtOptions, jwtVerifyCallback );
+
+//LOCAL STRATEGY
+let localOptions = {
+    usernameField: 'email',
+    passReqToCallback: true
+}
+
+let localVerifyCallback = async( email, password, done ) => {
+    try {
+        let user = await UserModel.findOne({[ EMAIL ] : email })
+        if( !user ) return done( null, false );
+
+        let passwordMatch = user.vaidatePassword( password );
+        if( !passwordMatch ) return done( null, false );
+        return done( null, user );
+    } catch (error) {
+        done( error, false )
+    }
+};
+let localStrategy = new LocalStrategy( localOptions, localVerifyCallback );
 
 module.exports = {
-    jwtStrategy
+    jwtStrategy,
+    localStrategy
 }
 
