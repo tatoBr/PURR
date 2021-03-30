670616e65324a2c4385d1a587ac0b1130d22ef72
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-03-30 09:19:25 -0300

    add 'singin method' field to user model

--- src/models/user.js
+++ src/models/user.js
@@ -4,10 +4,16 @@ const { Schema } = mongoose;
 const bcrypt = require( 'bcrypt' );
 
 const { globals:{ SALT_ROUNDS }, models:{ user, message:{ MODEL_NAME: MESSAGE }}} = require('../utils/constants' );
+
 const { MODEL_NAME: USER } = user;
-const { EMAIL, USERNAME, PASSWORD, MESSAGE_POOL, RECEIVED, SENT, FAVORITE_MESSAGES, FRIENDS, BLOCKED_USERS, LOGIN_ATTEMPTS, LOGIN_WAITING_TIME, REFRESH_TOKEN, drafts } = user.fields;
+const { EMAIL, USERNAME, PASSWORD, MESSAGE_POOL, RECEIVED, SENT, FAVORITE_MESSAGES, FRIENDS, BLOCKED_USERS, signInMethod, LOGIN_ATTEMPTS, LOGIN_WAITING_TIME, REFRESH_TOKEN, drafts } = user.fields;
+
 const { DOC_NAME: DRAFTS } = drafts;
 const { TITLE, BODY} = drafts.fields;
+
+const { DOC_NAME: SIGNIN_METHOD } = signInMethod;
+const { LOCAL, GOOGLE, FACEBOOK } = signInMethod.enumerators;
+
 
 const schema = new Schema({
     _id:{
@@ -42,6 +48,10 @@ const schema = new Schema({
     [FAVORITE_MESSAGES]: [{ type: Schema.Types.ObjectId, ref: MESSAGE }],
     [FRIENDS]: [{ type: Schema.Types.ObjectId, ref: USER }],
     [BLOCKED_USERS]: [{ type: Schema.Types.ObjectId, ref: USER }],
+    [SIGNIN_METHOD]:{
+        type: String,
+        enum: [ LOCAL, GOOGLE, FACEBOOK ],
+    },
     [LOGIN_ATTEMPTS]: {
         type: Schema.Types.Number,
         default: 0,
