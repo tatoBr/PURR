77d42e0a6665af1948c2b581525856b29dfbef48
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-03-30 17:27:38 -0300

    add google oauth strategy

--- src/utils/strategies.js
+++ src/utils/strategies.js
@@ -1,3 +1,6 @@
+const UsersServices = require('../services/users' );
+const usersServices = new UsersServices();
+
 const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt');
 const { Strategy: LocalStrategy } = require( 'passport-local');
 const GoogleOAuthStrategy = require( 'passport-google-plus-token' );
@@ -6,7 +9,18 @@ const { ACCESS_TOKEN_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require(
 const UserModel = require('../models/user');
 
 const { models:{ user }} = require('../utils/constants' );
-const { EMAIL } = user.fields;
+
+const { USERNAME, local, google, facebook, signInMethod } = user.fields;
+
+const { DOC_NAME: SIGNIN_METHOD }  = signInMethod
+const { DOC_NAME: LOCAL_DOC } = local
+const { DOC_NAME: GOOGLE_DOC } = google
+const { DOC_NAME: FACEBOOK_DOC } = facebook
+
+const { LOCAL, GOOGLE, FACEBOOK } = signInMethod.enumerators
+const { EMAIL: LOCAL_EMAIL, PASSWORD: LOCAL_PASSWORD } = local.fields
+const { ID: GOOGLE_ID, EMAIL: GOOGLE_EMAIL } = google.fields;
+const { ID: FACEBOOK_ID, EMAIL: FACEBOOK_EMAIL } = facebook.fields;
 
 //JSON WEB TOKENS STRATEGY
 let jwtOptions = {
@@ -44,11 +58,8 @@ let localOptions = {
  * @returns 
  */
 let localVerifyCallback = async( email, password, done ) => {
-    try {
-        console.log( email )
-        console.log( password )
-
-        let user = await UserModel.findOne({[ EMAIL ] : email })
+    try {        
+        let user = await UserModel.findOne({[`${LOCAL_DOC}.${LOCAL_EMAIL}`] : email })
         if( !user ) return done( null, false );
 
         let passwordMatch = user.vaidatePassword( password );
@@ -63,21 +74,42 @@ let localStrategy = new LocalStrategy( localOptions, localVerifyCallback );
 
 
 //GOOGLE OAUTH2 STRATEGY
-let oAuthOptions = {
+let googleOptions = {
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     passReqToCallback: true
 }
-let oAuthVerifyCallback = async( req, accessToken, refreshToken, profile, next) => {
-    console.log( 'Estou aqui;')
-    console.log( 'requisition', req );
-    console.log( 'access token', accessToken );
-    console.log( 'refresh token', refreshToken );
-    console.log( 'profile', profile );
-    next( null, false );
+let googleVerifyCallback = async( req, accessToken, refreshToken, profile, next) => {
+    try {
+        //check if user exist
+        let existingUser = await UserModel.findOne({[`${GOOGLE_DOC}.${GOOGLE_ID}`] : profile.id });
+        if( existingUser ) return next( new Error( UsersServices.USER_ALREADY_EXIST_ERRMSG ));
+
+        let data = {
+            [USERNAME]: req.body[ USERNAME ],
+            [SIGNIN_METHOD]: GOOGLE,           
+            [LOCAL_DOC]: { 
+                [LOCAL_EMAIL]: null,
+                [LOCAL_PASSWORD]: null
+            },
+            [GOOGLE_DOC]: {
+                [GOOGLE_ID]: profile.id,
+                [GOOGLE_EMAIL]: profile.emails[0].value
+            },
+            [FACEBOOK_DOC]: {
+                [FACEBOOK_ID]: null,
+                [FACEBOOK_EMAIL]: null
+            }
+        };
+        
+        let user = await usersServices.create( data );
+        next( null, user );
+    } catch (error) {
+        next( error, false );
+    }    
 }
 
-let oAuthStrategy = new GoogleOAuthStrategy( oAuthOptions, oAuthVerifyCallback );
+let oAuthStrategy = new GoogleOAuthStrategy( googleOptions, googleVerifyCallback );
 
 module.exports = {
     jwtStrategy,
